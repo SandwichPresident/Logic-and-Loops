e7f1829234b205827fb4c0365f5f618bf9407463
SandwichPresident <shb1713@rit.edu>
2025-10-05 19:46:52 -0400

    Added readline

--- Logic and Loops/Logic and Loops/Program.cs
+++ Logic and Loops/Logic and Loops/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string playerName = "adventurer";
+
+            Console.Write("Welcome to the jungle\nWe got fun and games\nWe got everything you want\nHoney, we know the names\n\n");
+            Console.WriteLine("Wait... I don't actually know your name.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
+
+            playerName = Console.ReadLine();
+            Console.WriteLine("Welcome to the jungle " + playerName + "!");
         }
     }
 }
