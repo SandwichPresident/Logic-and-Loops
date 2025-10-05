2740c66dfb271a02ab52882b616845bc4054d0aa
SandwichPresident <shb1713@rit.edu>
2025-10-05 19:50:29 -0400

    Added userChoice

--- Logic and Loops/Logic and Loops/Program.cs
+++ Logic and Loops/Logic and Loops/Program.cs
@@ -5,14 +5,19 @@
         static void Main(string[] args)
         {
             string playerName = "adventurer";
+            string userChoice = "none";
 
             Console.Write("Welcome to the jungle\nWe got fun and games\nWe got everything you want\nHoney, we know the names\n\n");
             Console.WriteLine("Wait... I don't actually know your name.");
             Console.WriteLine("What is your name?");
             Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
-
             playerName = Console.ReadLine();
             Console.WriteLine("Welcome to the jungle " + playerName + "!");
+            Console.WriteLine("Which path do you chose?");
+            Console.WriteLine("Type 'left' to choose the path and go left or type 'right' to choose the cave and go right.");
+            userChoice = Console.ReadLine();
+            Console.WriteLine("\nYou chose: " + userChoice + "\n");
+
         }
     }
 }
