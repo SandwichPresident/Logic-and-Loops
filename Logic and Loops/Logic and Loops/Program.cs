b16dbc31dc55e56bd89c0d0d444ac30959c7bd34
SandwichPresident <shb1713@rit.edu>
2025-10-05 19:45:24 -0400

    Created File and wrote starting code

---
+++ Logic and Loops/Logic and Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logic_and_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
